d4ea077fdad714674dd44ed10b6a7c75bff7aadc
Damien Metzler <dmetzler@nuxeo.com>
2017-10-18 09:53:29 -0700

    NXIO-749 customize kafka configuration

--- nuxeo-activity-injector/src/test/scala/org/nuxeo/ecm/ha/injector/CreateCheckDelete.scala
+++ nuxeo-activity-injector/src/test/scala/org/nuxeo/ecm/ha/injector/CreateCheckDelete.scala
@@ -19,16 +19,17 @@ object CreateCheckDelete {
             .body(StringBody("""{"entity-type": "document","type": "Note","title": "Test doc","name":"newdoc"}""")).asJSON
             .check(jsonPath("$.uid").saveAs("docId"))
         )
-        .pause(asyncPauseTime) // Pause for the async worker to update the proerty
+        .pause(8 seconds) // Pause for the async worker to update the proerty
         .tryMax(4, "loopIndex") { // Loop for additional 10s max
-          pause(session => computePause(session("loopIndex").as[Int]))
-          .exec(
+          
+          exec(
             http("Step 2 - Retrieve document")
               .get("/nuxeo/api/v1/id/${docId}")
               .headers(HaHeader.default)
-              .check(jsonPath("$['properties']['dc:lastContributor']").is("Administrator"))
+              .check(jsonPath("$['properties']['dc:description']").is("updated"))
               .basicAuth("${userId}","${userId}")              
-          )                            
+          )
+          .pause(session => computePause(session("loopIndex").as[Int]))                            
         }
         /*.exitBlockOnFail{
           pause(1)
@@ -38,13 +39,13 @@ object CreateCheckDelete {
             .basicAuth("${userId}","${userId}")                        
           )
         }  */      
-        .pause(5)
+        /*.pause(5)
         .exec(
           http("Step 3 - Delete document")
             .delete("/nuxeo/api/v1/id/${docId}")
             .headers(HaHeader.default)
             .basicAuth("${userId}","${userId}")                        
-          )
+          )*/
         
     }
   }
