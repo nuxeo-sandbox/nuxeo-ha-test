8430362a576347d21cadf6d7eb5fd3e8e23f4a8a
Damon Brown <dbrown@nuxeo.com>
2018-02-28 13:30:38 -0500

    Add DR gatling scenario with elasticsearch probe, update long async test to fix race condition

--- nuxeo-ha-testartifact/nuxeo-ha-testartifact-core/src/test/java/org/nuxeo/ecm/test/LongAsynchronousUpdaterTest.java
+++ nuxeo-ha-testartifact/nuxeo-ha-testartifact-core/src/test/java/org/nuxeo/ecm/test/LongAsynchronousUpdaterTest.java
@@ -2,6 +2,9 @@ package org.nuxeo.ecm.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
@@ -10,10 +13,13 @@ import org.junit.runner.RunWith;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.core.event.EventService;
 import org.nuxeo.ecm.core.test.CoreFeature;
 import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.ecm.core.work.api.WorkManager;
+import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
@@ -31,24 +37,30 @@ import org.nuxeo.runtime.transaction.TransactionHelper;
 @Deploy("org.nuxeo.ecm.ha.nuxeo-ha-testartifact")
 public class LongAsynchronousUpdaterTest {
 
-    @Inject
-    protected CoreSession session;
+  @Inject
+  protected CoreSession session;
 
-    @Test
-    public void propertyIsUpdateAsynchronouslyAndAfterALongTime() throws Exception {
-        DocumentModel doc = session.createDocumentModel("/", "sample", "Note");
-        doc = session.createDocument(doc);
-        assertNull(doc.getProperty("dc:description").getValue(String.class));
-        TransactionHelper.commitOrRollbackTransaction();
+  @Inject
+  WorkManager workManager;
 
-        Thread.sleep(6000);
+  @Test
+  public void propertyIsUpdateAsynchronouslyAndAfterALongTime() throws Exception {
+    DocumentModel doc = session.createDocumentModel("/", "sample", "Note");
+    doc = session.createDocument(doc);
+    assertNull(doc.getProperty("dc:description").getValue(String.class));
 
-        TransactionHelper.startTransaction();
-        doc = session.getDocument(new PathRef("/sample"));
-        assertEquals("updated", doc.getProperty("dc:description").getValue(String.class));
+    waitForWorkers();
 
+    doc = session.getDocument(new PathRef("/sample"));
+    assertEquals("updated", doc.getProperty("dc:description").getValue(String.class));
 
+  }
 
-
-    }
+  protected void waitForWorkers() throws InterruptedException {
+    TransactionHelper.commitOrRollbackTransaction();
+    TransactionHelper.startTransaction();
+    
+    final boolean allCompleted = workManager.awaitCompletion(10, TimeUnit.SECONDS);
+    assertTrue(allCompleted);
+  }
 }
