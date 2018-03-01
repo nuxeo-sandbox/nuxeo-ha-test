874711bda1efabccb2c7c28f0cb38bcec31bd393
Damon Brown <dbrown@nuxeo.com>
2018-03-01 11:51:21 -0500

    Merge DR test with HA and add additional functions

--- nuxeo-activity-injector/README.md
+++ nuxeo-activity-injector/README.md
@@ -4,5 +4,11 @@ That project is basically a gatling test. To execute :
 
 
 	# mvn clean install
-	# java -jar target/nuxeo-activity-injector-1.0-SNAPSHOT.jar
+	# java [-Dsysprop=...]... -jar target/nuxeo-activity-injector-1.0-SNAPSHOT.jar
 
+System Properites for HA/DR Test:
+- primary: Primary URL (default: http://nuxeo.apps.io.nuxeo.com)
+- secondary: Secondary URL - set to test DR capabilities, assumes hot-standby
+- users: Number of users to simulate (default: 20)
+- ramp: Second to ramp up users (default: 30)
+- duration: Number of seconds to run the simulation (defualt: 300)
--- nuxeo-activity-injector/src/main/scala/org/nuxeo/ecm/ha/injector/HaInjector.scala
+++ nuxeo-activity-injector/src/main/scala/org/nuxeo/ecm/ha/injector/HaInjector.scala
@@ -5,37 +5,32 @@ import io.gatling.http.Predef._
 import io.gatling.http.request.builder._
 import scala.util.Random
 
-
-
 class HaInjector extends Simulation {
 
-
-
-  val url = System.getProperty("url", "http://nuxeo.apps.io.nuxeo.com")
+  val primary = System.getProperty("primary", "http://nuxeo.apps.io.nuxeo.com")
+  val secondary = System.getProperty("secondary", null)
   val nbUsers = Integer.getInteger("users", 20)
   val myRamp = java.lang.Long.getLong("ramp", 30L)
   val myDuration = java.lang.Long.getLong("duration", 300L)
-  
+
   val httpProtocol = http
-    .baseURL(url)
+    .baseURL(primary)
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0")
 
-  val default = scenario("default").during(myDuration) {exec(CreateCheckDelete.scenario(2))}
-
+  val default = scenario("HA/DR").during(myDuration) { exec(CreateCheckDelete.scenario(primary, secondary)) }
 
   before {
     println("Simulation is about to start!")
- 
+
   }
 
   after {
-                      
+
     println("Simulation is finished!")
   }
 
   setUp(
-    default.inject( rampUsers(nbUsers) over (myRamp seconds))    
-  ).protocols(httpProtocol)
+    default.inject(rampUsers(nbUsers) over (myRamp seconds))).protocols(httpProtocol)
 
 }
