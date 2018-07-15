42cd7817eb126a9d5e07cd90689e2f6f9d86ebba
Damon Brown <dbrown@nuxeo.com>
2018-07-14 22:09:46 -0400

    Tweak some settings and bump version

--- nuxeo-ha-testartifact/nuxeo-ha-testartifact-core/src/main/java/org/nuxeo/ecm/ha/UpdateNoteListener.java
+++ nuxeo-ha-testartifact/nuxeo-ha-testartifact-core/src/main/java/org/nuxeo/ecm/ha/UpdateNoteListener.java
@@ -4,17 +4,32 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.event.EventBundle;
 import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.EventListener;
+import org.nuxeo.ecm.core.event.PostCommitEventListener;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.core.work.api.Work;
 import org.nuxeo.ecm.core.work.api.WorkManager;
 import org.nuxeo.ecm.core.work.api.WorkManager.Scheduling;
 import org.nuxeo.runtime.api.Framework;
 
-public class UpdateNoteListener implements EventListener {
+public class UpdateNoteListener implements EventListener, PostCommitEventListener {
 
     private static final Log log = LogFactory.getLog(DocUpdaterWork.class);
+
+    private final Object mutex = new Object();
+
+    private WorkManager workManager = null;
+
+    private String updateType = "Note";
+
+    @Override
+    public void handleEvent(EventBundle events) {
+        for (Event event : events) {
+            handleEvent(event);
+        }
+    }
 
     @Override
     public void handleEvent(Event event) {
@@ -23,13 +38,21 @@ public class UpdateNoteListener implements EventListener {
             return;
         }
 
-        String updateType = Framework.getProperty("nuxeo.ha.listener.updateType", "Note");
+        synchronized (mutex) {
+            if (workManager == null) {
+                workManager = Framework.getService(WorkManager.class);
+                updateType = Framework.getProperty("nuxeo.ha.listener.updateType", "Note");
+            }
+        }
+
         DocumentEventContext docCtx = (DocumentEventContext) ctx;
         DocumentModel doc = docCtx.getSourceDocument();
         if (updateType.equals(doc.getType())) {
-            log.debug("Performing ASYNC update: " + doc.getId() + "@" + doc.getVersionLabel() + ", " + event.getName());
+            if (log.isDebugEnabled()) {
+                log.debug("Performing update: " + doc.getId() + "@" + doc.getVersionLabel() + ", " + event.getName());
+            }
             Work work = new DocUpdaterWork(doc.getRepositoryName(), doc.getId());
-            Framework.getService(WorkManager.class).schedule(work, Scheduling.ENQUEUE, true);
+            this.workManager.schedule(work, Scheduling.ENQUEUE, true);
         }
     }
 }
