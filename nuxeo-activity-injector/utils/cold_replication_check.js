7d9ffce3d9a7d48233d722f06c543b1efb127a15
Damon Brown <dbrown@nuxeo.com>
2018-07-30 17:31:19 -0400

    Add event driven computations

--- nuxeo-activity-injector/utils/cold_replication_check.js
+++ nuxeo-activity-injector/utils/cold_replication_check.js
@@ -2,76 +2,85 @@ const connect = require('./connect.js');
 const colors = require('colors/safe');
 const fs = require('fs');
 const readline = require('readline');
-const stream = require('stream');
+const Stream = require('stream');
 
 const Queue = require('better-queue');
 
-var tick = 0,
+let tick = 0,
+  remaining = 0,
   blobok = 0,
   blobgone = 0,
   docok = 0,
   docgone = 0;
 
-const q = new Queue(function (task, done) {
-  tick += 1;
-  if (tick % 100 === 0) {
-    console.log(`${tick}: ${docok}/${docgone}; ${blobok}/${blobgone} -${q.size()}`);
-  }
-
+const q = new Queue((task, done) => {
   const parts = task.split(',');
-  connect.primary.repository().fetch(parts[0]).then(function (doc) {
-    // console.log(colors.green("doc:" + parts[0]));
-    docok += 1;
+  connect.primary.repository().fetch(parts[0]).then((doc) => {
+    if (doc) {
+      docok += 1;
+    }
     connect.primary.operation('Blob.VerifyBinaryHash')
       .param('digest', parts[1])
-      .execute().then(function (result) {
+      .execute().then((result) => {
         if (result.value === parts[1]) {
-          // console.log(colors.green("blob:" + parts[1]));
           blobok += 1;
         } else {
-          //console.log(colors.red("noblob:" + parts[1]));
+          console.log(colors.red(`noblob:${parts[1]}`));
           blobgone += 1;
         }
         done();
-      }).catch(function (err) {
+      })
+      .catch(() => {
+        console.log(colors.red(`blobfail:${parts[1]}`));
         blobgone += 1;
         done();
       });
-  }).catch(function (error) {
+  }).catch((error) => {
     docgone += 1;
-    //    console.log(colors.red("nodoc:" + parts[0] + ',' + error));
+    console.log(colors.red(`nodoc:${parts[0]},${error}`));
     connect.primary.operation('Blob.VerifyBinaryHash')
       .param('digest', parts[1])
-      .execute().then(function (result) {
+      .execute().then((result) => {
         if (result.value === parts[1]) {
-          // console.log(colors.green("blob:" + parts[1]));
           blobok += 1;
         } else {
-          //console.log(colors.red("noblob:" + parts[1]));
+          console.log(colors.red(`noblob:${parts[1]}`));
           blobgone += 1;
         }
         done();
-      }).catch(function (err) {
+      })
+      .catch(() => {
+        console.log(colors.red(`blobfail:${parts[1]}`));
         blobgone += 1;
         done();
       });
   });
 }, {
-  concurrent: 6
+  concurrent: 10,
 });
 
-q.on('drain', function () {
+q.on('task_queued', () => {
+  remaining += 1;
+});
+q.on('task_finish', () => {
+  remaining -= 1;
+  tick += 1;
+  if ((tick % 100) === 0) {
+    console.log(colors.gray(`${tick}: ${docok}/${docgone}; ${blobok}/${blobgone} -${remaining}`));
+  }
+});
+q.on('drain', () => {
   console.log(colors.green(`${tick}: ${docok}/${docgone}; ${blobok}/${blobgone}`));
 });
 
-var currentLine = null;
+let currentLine = null;
 
 function processFile(inputFile) {
   const instream = fs.createReadStream(inputFile);
-  const outstream = new(stream)();
+  const outstream = new(Stream)();
   const rl = readline.createInterface(instream, outstream);
 
-  rl.on('line', function (line) {
+  rl.on('line', (line) => {
     if (currentLine) {
       q.push(line);
     }
